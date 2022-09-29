7b8168432b42b71c1c5af7e3bb1541fe2dcae25f
Tyler Watson <tylerww91@gmail.com>
2022-09-29 11:42:42 -0700

    adds html, minor css styling, createItem(), displayErrors(), addItemForm eventlistener and its necessay DOM elements. Now useres are able to add items to their shopping list which is currently stored in supabase

--- app.js
+++ app.js
@@ -2,10 +2,41 @@
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
 
+import { createItem } from './fetch-utils.js';
 /* Get DOM Elements */
-
+const addItemForm = document.getElementById('add-item-form');
+const errorDisplay = document.getElementById('error-display');
 /* State */
-
+let items = [];
+let error = null;
 /* Events */
 
+addItemForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(addItemForm);
+    const newItem = {
+        item: formData.get('item'),
+    };
+
+    const response = await createItem(newItem);
+    error = response.error;
+    const item = response.data;
+
+    if (error) {
+        displayError();
+    } else {
+        items.push(item);
+        addItemForm.reset();
+    }
+});
+
 /* Display Functions */
+
+function displayError() {
+    if (error) {
+        errorDisplay.textContent = error.message;
+    } else {
+        errorDisplay.textContent = '';
+    }
+}
